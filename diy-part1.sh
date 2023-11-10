08e41444773feddf1a446b63136e5a1164cb484f
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2023-11-10 21:44:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,9 +9,8 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 
-
+nuexini https://github.com/NueXini/NueXini_Packages
 # Uncomment a feed source
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small1 https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git nuexini https://github.com/NueXini/NueXini_Package' feeds.conf.default
 sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
