8588185cfdd5c08ad6da5245eafa151ae6a6b89a
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2023-11-10 19:00:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,20 +10,7 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-feed() {
-    if [ -n "$3" ] ;then
-        echo "src-git $1 $2;$3" >>feeds.conf.default
-    else
-        echo "src-git $1 $2" >>feeds.conf.default
-    fi
-}
-
 # Uncomment a feed source
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small1 https://github.com/kenzok8/small' feeds.conf.default
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-#feed helloworld https://github.com/fw876/helloworld
-#feed turbocc https://github.com/chenmozhijin/turboacc.git luci
-
-feed nuexini https://github.com/NueXini/NueXini_Packages.git
-feed kiddin9 https://github.com/kiddin9/openwrt-packages.git
