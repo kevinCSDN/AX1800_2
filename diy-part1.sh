b191766daa66f33087a97750ce81829b2be424b3
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2023-11-11 15:17:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,9 +9,9 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 
-
+# 样板 echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
 # Uncomment a feed source
 # sed -i '$a src-git nuexini https://github.com/NueXini/NueXini_Package' feeds.conf.default
-echo 'src-git helloworld https://github.com/NueXini/NueXini_Packages/luci-app-xunlei' >>feeds.conf.default
+echo 'src-git luci-app-xunlei https://github.com/NueXini/NueXini_Packages/luci-app-xunlei.git' >>feeds.conf.default
 sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
