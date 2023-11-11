d8036d4c552bcde7fa8597354edf4616b7ccfb5f
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2023-11-11 16:35:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,6 @@
 # Uncomment a feed source
 # git clone https://github.com/gngpp/luci-theme-design.git package/luci-theme-design
 # sed -i '$a src-git nuexini https://github.com/NueXini/NueXini_Package' feeds.conf.default
-echo 'src-git luci-app-xunlei https://github.com/NueXini/NueXini_Packages/luci-app-xunlei.git' >>feeds.conf.default
-sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+ git clone https://github.com/NueXini/NueXini_Packages/luci-app-xunlei.git package/luci-app-xunlei
+ sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
